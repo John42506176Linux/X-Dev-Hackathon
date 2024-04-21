881b8e4d8f3c494383696e7ccaf4d55a361a6245
tylerjheslop <tylerjheslop@gmail.com>
2024-04-21 00:01:02 -0700

    Twitter explore page with shuffling topic blocks

--- frontend/app/explore/page.tsx
+++ frontend/app/explore/page.tsx
@@ -1,10 +1,21 @@
 'use client'
 
-import * as React from "react"
+import React, { useState, useEffect } from 'react'
+import { useTransition, animated } from '@react-spring/web'
+import shuffle from "lodash.shuffle"
 import Image from "next/image"
-
-// @ts-ignore
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Link from "next/link";
 import {ScrollArea, ScrollBar} from '@/components/ui/scroll-area'
+import {resolveViewport} from "next/dist/lib/metadata/resolvers/resolve-basics";
 
 export interface Artwork {
   artist: string
@@ -17,32 +28,37 @@ export interface Tweet {
   handle: string
   content: string
   timestamp: string
+  likes: number
 }
 
-export const tweets: Tweet[] = [
+// @nextjs-ignore
+const tweets: Tweet[] = [
   {
     profilePic: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
     userName: "$GMale",
     handle: "laptopcrust",
     content: "thinkin of my next tweet",
-    timestamp: "Today at 3:32 PM"
+    timestamp: "Today at 3:32 PM",
+    likes: 0
+  },
+  {
+    profilePic: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+    userName: 'Twitter',
+    handle: 'Twitter',
+    content: 'Happy 3rd anniversary #TBT! See how "Throwback Thursday" cemented its status as a weekly Twitter tradition:',
+    timestamp: '6:26 PM - Apr 30, 2015',
+    likes: 0
   },
   {
     profilePic: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
     userName: "$GMale",
     handle: "laptopcrust",
     content: "thinkin of my next tweet",
-    timestamp: "Today at 3:32 PM"
-  },
-  {
-    profilePic: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-    userName: "$GMale",
-    handle: "laptopcrust",
-    content: "thinkin of my next tweet",
-    timestamp: "Today at 3:32 PM"
+    timestamp: "Today at 3:32 PM",
+    likes: 0
   },
 ]
-export const works: Artwork[] = [
+const works: Artwork[] = [
   {
     artist: "Ornella Binni",
     art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
@@ -57,22 +73,22 @@ export const works: Artwork[] = [
   },
 ]
 
-class SocialMediaPost extends React.Component<({ profilePic: any, userName: any, handle: any, content: any, timestamp: any })> {
+class TwitterPost extends React.Component<({ profilePic: any, userName: any, handle: any, content: any, timestamp: any })> {
   render() {
     let {profilePic, userName, handle, content, timestamp} = this.props;
     return (
-      <div className="flex border border-gray-200 p-4 rounded-lg mb-5">
+      <div className="flex min-h-40 place-items-center border border-gray-200 p-4 rounded-lg mb-5">
         <img
           src={profilePic}
           alt={`${userName}'s profile pic`}
           className="w-12 h-12 rounded-full mr-4"
         />
         <div className="flex-1">
-          <div className="flex items-center mb-2">
-            <span className="font-bold mr-2">{userName}</span>
-            <span className="text-gray-500">{handle}</span>
+          <div className="flex items-center">
+            <a href="" className="font-bold mr-2">{userName}</a>
+            <a href="" className="text-gray-500">@{handle}</a>
           </div>
-          <p>{content}</p>
+          <p className="max-w-sm place-content-center text-wrap mb-2 mt-2">{content}</p>
           <span className="text-gray-500 text-sm">{timestamp}</span>
         </div>
       </div>
@@ -80,44 +96,136 @@ class SocialMediaPost extends React.Component<({ profilePic: any, userName: any,
   }
 }
 
-const postData = {
-  profilePic: '...', // Replace with actual image URL
-  userName: 'Twitter',
-  handle: '@Twitter',
-  content: 'Happy 3rd anniversary #TBT! See how "Throwback Thursday" cemented its status as a weekly Twitter tradition:',
-  timestamp: '6:26 PM - Apr 30, 2015',
-};
-export default function ExplorePage() {
+
+
+class ExploreCard extends React.Component<({profilePic: any, userName: any, handle: any, content: any, timestamp: any })> {
+  render() {
+    let {profilePic, userName, handle, content, timestamp} = this.props;
+    return (
+      <Card className="sm:col-span-2">
+        <CardHeader className="pb-3">
+          <CardTitle>#{handle}</CardTitle>
+          <CardDescription className="max-w-lg text-balance leading-relaxed">
+            This is a brief description of the topic.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+
+        </CardContent>
+        <CardFooter>
+          <Button>Explore</Button>
+        </CardFooter>
+      </Card>
+    );
+  }
+}
+
+export function TopicBlock() {
+  // useEffect(()=> {
+  //   window.addEventListener('resize', ()=> {
+  //     // console.log(window.innerHeight, window.innerWidth)
+  //   })
+  // }, [])
+  // @ts-ignore
   return (
-    <ScrollArea className="w-full whitespace-nowrap rounded-md border">
-      {/*<div className="flex w-max space-x-4 p-4">*/}
-      <div className="flex w-max space-x-4 p-4">
-        {works.map((artwork) => (
-          <figure key={artwork.artist} className="shrink-0">
-            <div className="overflow-hidden rounded-md">
-              <Image
-                src={artwork.art}
-                alt={`Photo by ${artwork.artist}`}
-                /*set w-[250px] to a variable that updates for desktop + mobile*/
-                className="space-y-3 h-[375px]"
-                // className="aspect-[3/4] h-fit w-fit object-cover"
-                width={300}
-                height={400}
-              />
-            </div>
-            <figcaption className="pt-2 text-xs text-muted-foreground">
-              Photo by{" "}
-              <span className="font-semibold text-foreground">
+    <ScrollArea className="ease-in w-full whitespace-nowrap rounded-md border">
+      <Card className="sm:col-span-2">
+        <CardHeader className="pb-3">
+          <CardTitle>#jjk</CardTitle>
+          <CardDescription className="max-w-lg text-balance leading-relaxed">
+            Gege robs his fans.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex w-max space-x-4 p-4">
+            {works.map((artwork) => (
+              <figure key={artwork.artist} className="shrink-0">
+                <div className="overflow-hidden rounded-md">
+                  <Image
+                    src={artwork.art}
+                    alt={`Photo by ${artwork.artist}`}
+                    /*set w-[250px] to a variable that updates for desktop + mobile*/
+                    className="space-y-3 h-[225px] w-[225px]"
+                    width={225}
+                    height={225}
+                  />
+                </div>
+                <figcaption className="pt-2 text-xs text-muted-foreground">
+                  Photo by{" "}
+                  <span className="font-semibold text-foreground">
                 {artwork.artist}
               </span>
-            </figcaption>
-          </figure>
-        ))}
-        {tweets.map((tweet) => (
-          <SocialMediaPost key={tweet.content} {...tweet} />
-        ))}
-      </div>
+                </figcaption>
+              </figure>
+            ))}
+
+            {tweets.map((tweet) => (
+              <><div className="shrink place-content-center">
+                <TwitterPost key={tweet.content} {...tweet} />
+              </div></>
+            ))}
+            <div className="flex min-h-40 place-items-center p-4 rounded-lg mb-5">
+              <Button className="shrink place-content-center min-h-40 place-items-center">See more</Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
       <ScrollBar orientation="horizontal"/>
     </ScrollArea>
   )
+}
+
+let data = [
+  { id: 1, content: <TopicBlock />, rank: 3 },
+  { id: 2, content: <TopicBlock />, rank: 1 },
+  { id: 3, content: <TopicBlock />, rank: 2 },
+
+  // ... more elements, initially sorted by rank
+];
+
+export function List() {
+  const [rows, set] = useState(data)
+  useEffect(() => {
+    const t = setInterval(() => set(shuffle), 2000)
+    return () => clearInterval(t)
+  }, [])
+
+  let height = 0
+  let heightCard = 400
+  const transitions = useTransition(
+    rows.map(data => ({ ...data, y: (height += heightCard) - heightCard })),
+    {
+      key: (item: any) => item.id,
+      from: { height: 0, opacity: 0 },
+      leave: { height: 0, opacity: 0 },
+      enter: ({ y, height = 500 }) => ({ y, height, opacity: 1 }),
+      update: ({ y, height = 500 }) => ({ y, height }),
+    }
+  )
+
+  return (
+    <div className="list" style={{ height }}>
+      {transitions((style, item, t, index) => (
+        <animated.div className="card" style={{ zIndex: data.length - index, ...style }}>
+          <div className="cell">
+            <TopicBlock />
+          </div>
+        </animated.div>
+      ))}
+    </div>
+  )
+}
+
+export default function ExplorePage() {
+    // const [transitions, api] = useTransition(data, () => ({
+    //   from: { opacity: 0 },
+    //   enter: { opacity: 1 },
+    //   leave: { opacity: 1 },
+    // }))
+    //
+    // return transitions((style, item) => (
+    //   <animated.div style={style}>{item}</animated.div>
+    // ))
+  return <List />
+
 }
