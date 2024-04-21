cdd538a7ff6f9540e766c9464cc0933eaa606563
Vidith Balasa <vidith@squareup.com>
2024-04-21 01:09:23 -0700

    Finished http endpoint

--- api/main.py
+++ api/main.py
@@ -1,12 +1,16 @@
+import os
 from fastapi import FastAPI
-from user_tweets import get_tweets_and_likes
+from user_tweets import get_tweets_and_likes, get_twitter_user_by_username
 from text_embedding import get_text_embedding
 from topic_modeling import generate_topics
+from upload_to_pinecone import upload_embedding
 
 app = FastAPI()
 
 @app.get("/username")
 async def get_username(username: str):
+    bearer_token = os.environ["TWITTER_BEARER_TOKEN"]
+    user_id = get_twitter_user_by_username(username, bearer_token)['data']['id']
     tweets = get_tweets_and_likes(username)
 
     embeddings = []
@@ -19,5 +23,15 @@ async def get_username(username: str):
 
     topic_embeddings = generate_topics(embeddings, text_list)
 
-    return {"num_topics": len(topic_embeddings)}
+    pinecone_values = list()
+    for i, emb in enumerate(topic_embeddings):
+        pinecone_values.append({
+            'id': str(i),
+            'metadata': {'user_id': str(user_id)},
+            'values': emb
+        })
+
+    upload_embedding(pinecone_values)
+
+    return {"success": True}
 
--- api/upload_to_pinecone.py
+++ api/upload_to_pinecone.py
@@ -1,6 +1,9 @@
+import os
+from pinecone import Pinecone
+
 def upload_embedding(vectors):
     api_key = os.environ["PINECONE_API_KEY"]
     pinecone = Pinecone(api_key=api_key)
 
-    index = pinecone.Index('text-index')
+    index = pinecone.Index('test-index')
     index.upsert(vectors=vectors)
